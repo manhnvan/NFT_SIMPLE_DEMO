f84c4f30e41ed9c3c247c4144a5bfabdb5178e3b
Cao Chinh Bùi <buicaochinh0811@gmail.com>
2021-09-15 13:51:16 +0700

    add jelly

--- client/scripts/randomEnemies.js
+++ client/scripts/randomEnemies.js
@@ -3,16 +3,18 @@ const randomEnemies = () => {
     for (let i = 0; i < 6; i++) {
         roadEnemies.push({
             id: Math.floor((Math.floor(Math.random() * 1024) * Math.random()) % 6),
-            pos: 300 + i*150
+            pos: 300 + i * 150 + Math.random() * 90
         })
     }
     return roadEnemies
 }
 
 const renderEnemy = (id, posX, posY) => {
-    const enemy = new PIXI.Sprite(PIXI.Texture.WHITE)
+    const yellowJelly = new PIXI.Texture.from(`../assets/jelly/Jelly (${id + 1}).png`)
+    const enemy = new PIXI.Sprite(yellowJelly)
     enemy.anchor.set(0.5)
     enemy.position.set(posX, posY)
-    enemy.tint = '0x000000'
+    enemy.width = 32
+    enemy.height = 32
     runScreen.addChild(enemy)
 }
